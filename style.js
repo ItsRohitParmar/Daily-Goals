13b01853c14a7a7afc9ea999d9a6d09f1613672f
Rohit <rohitparmar@gmail.com>
2023-02-08 15:23:11 +0530

    Daily Goals with new Style

--- style.js
+++ style.js
@@ -1,59 +1,78 @@
-const taskName = document.getElementById("taskName");
-const task = document.getElementById("task");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
-
-const taskArray = localStorage.getItem(task) ? JSON.parse(localStorage.getItem(task)) : [];
+// console.log(localStorage.getItem(something));
+const task = localStorage.getItem("task") ? JSON.parse(localStorage.getItem("task")) : [];
 
 showAllTask();
 
 
-addBtn.addEventListener("click", () => {
+addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    console.log("submited");
     removeAllTask();
 
-    let title = taskName.value;
-    let description = task.value;
-    taskArray.push({ title, description });
-    console.log(taskArray);
+    task.push({ title: title.value, description: description.value });
 
-    localStorage.setItem(task, JSON.stringify(taskArray));
+    localStorage.setItem("task", JSON.stringify(task));
     showAllTask();
 })
 
+
+
 function showAllTask() {
-    taskArray.forEach((value, index)=>{
-        const taskcontainer = document.createElement("div");
-        taskcontainer.setAttribute("class", "taskcontainer")
+    task.forEach((value, index)=>{
+        const taskDiv = document.createElement("div");
+        taskDiv.setAttribute("class", "task");
 
+        const div = document.createElement("div");
+        
         const p = document.createElement("p");
-        p.setAttribute("class", "task");
-        p.innerText = value.description;
-        taskcontainer.append(p);
+        p.setAttribute("class", "title");
+        p.innerText = value.title;
+        div.append(p);
 
-        const img = document.createElement("img");
-        img.setAttribute("src", "./Icons/remove.png");
-        img.setAttribute("alt", "remove");
-        taskcontainer.append(img);
+        const span = document.createElement("span");
+        span.setAttribute("class", "description");
+        span.innerText = value.description;
+        div.append(span);
 
-        img.addEventListener("click", () => {
+        taskDiv.append(div);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("class", "delete");
+        deleteBtn.setAttribute("type", "submit");
+        deleteBtn.innerText = "-";
+        taskDiv.append(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => {
             removeAllTask();
-            taskArray.splice(index, 1);
-            console.log(taskArray);
+            task.splice(index, 1);
+            console.log(task);
 
-            localStorage.setItem(task, JSON.stringify(taskArray));
+            localStorage.setItem("task", JSON.stringify(task));
 
             showAllTask();
         })
 
-        taskList.prepend(taskcontainer);
+        deleteBtn.addEventListener('mouseover', ()=>{
+            taskDiv.style.backgroundColor = "green";
+        })
+
+        deleteBtn.addEventListener('mouseout', ()=>{
+            taskDiv.style.backgroundColor = "";
+        })
+
+        taskList.prepend(taskDiv);
     })
 }
 
 
 function removeAllTask(){
-    taskArray.forEach((value , index)=>{
-        const div =  document.querySelector(".taskcontainer");
+    task.forEach(()=>{
+        const div =  document.querySelector(".task");
         div.remove();
     })
 }
